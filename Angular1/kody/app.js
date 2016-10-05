44e5131363c87fb36a68c4f9b9baf21d29233253
adziobek <adziobek@iso.pl>
2016-10-05 12:38:00 +0200

    Dodanie prostego kontrolera

--- Angular1/kody/app.js
+++ Angular1/kody/app.js
@@ -1,3 +1,9 @@
 /**
  * Created by adziobek on 05.10.2016.
  */
+
+angular.module("myapp", []).controller('SimpleController', function ($scope) {
+
+    $scope.greeting = "Hello from AngularJS";
+
+})
